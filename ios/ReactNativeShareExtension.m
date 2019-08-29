bf87bd751598410e66960b4390d29c988d0df45d
Nick Badagliacca <nick@terraeclipse.com>
2019-08-29 14:20:45 -0700

    Better treatment of item attachments array and some logging

--- ios/ReactNativeShareExtension.m
+++ ios/ReactNativeShareExtension.m
@@ -55,19 +55,17 @@ RCT_REMAP_METHOD(data,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    [self extractDataFromContext: extensionContext withCallback:^(NSString* val, NSString* contentType, NSException* err) {
+    [self extractDataFromContext: extensionContext withCallback:^(NSDictionary* dict, NSException* err) {
+        NSLog(@"react-native-share-extension dict: %@", dict);
         if(err) {
             reject(@"error", err.description, nil);
         } else {
-            resolve(@{
-                      @"type": contentType,
-                      @"value": val
-                      });
+            resolve(dict);
         }
     }];
 }
 
-- (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSString *value, NSString* contentType, NSException *exception))callback {
+- (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSDictionary* dict, NSException *exception)) callback {
     @try {
         NSExtensionItem *item = [context.inputItems firstObject];
         NSArray *attachments = item.attachments;
@@ -79,51 +77,55 @@ RCT_REMAP_METHOD(data,
         [attachments enumerateObjectsUsingBlock:^(NSItemProvider *provider, NSUInteger idx, BOOL *stop) {
             if([provider hasItemConformingToTypeIdentifier:URL_IDENTIFIER]) {
                 urlProvider = provider;
-                *stop = YES;
-            // Note: temporarily removing imageProvider handling because the
-            // existing implementation treated all attachments with identifier
-            // `public.image` as a filepath/url, when in fact a number of apps send
-            // a UIImage with the same identifier (which causes a crash). @todo:
-            // fix for this in native
             } else if ([provider hasItemConformingToTypeIdentifier:TEXT_IDENTIFIER]){
                 textProvider = provider;
-                *stop = YES;
             }
         }];
+
+        __block NSMutableDictionary* dict = [[NSMutableDictionary alloc] init];
+        __block BOOL ok = false;
 
         if(urlProvider) {
             [urlProvider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
                 NSURL *url = (NSURL *)item;
 
-                if(callback) {
-                    callback([url absoluteString], @"text/plain", nil);
-                }
+                [dict setObject:[url absoluteString] forKey:@"url"];
             }];
-        } else if (imageProvider) {
+            ok = true;
+        }
+        if (imageProvider) {
             [imageProvider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
                 NSURL *url = (NSURL *)item;
 
-                if(callback) {
-                    callback([url absoluteString], [[[url absoluteString] pathExtension] lowercaseString], nil);
-                }
+                [dict setObject:[url absoluteString] forKey:[[[url absoluteString] pathExtension] lowercaseString]];
             }];
-        } else if (textProvider) {
+            ok = true;
+        }
+        if (textProvider) {
             [textProvider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
                 NSString *text = (NSString *)item;
 
-                if(callback) {
-                    callback(text, @"text/plain", nil);
-                }
+                [dict setObject:text forKey:@"text"];
             }];
-        } else {
-            if(callback) {
-                callback(nil, nil, [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]);
-            }
+            ok = true;
         }
+
+        if (!callback) return;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.4 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+            if (ok) {
+                if (callback) {
+                    callback(dict, nil);
+                }
+            } else {
+                if (callback) {
+                    callback(nil, [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]);
+                }
+            }
+        });
     }
     @catch (NSException *exception) {
         if(callback) {
-            callback(nil, nil, exception);
+            callback(nil, exception);
         }
     }
 }
